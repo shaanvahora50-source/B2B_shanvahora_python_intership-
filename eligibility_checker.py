8f1b9b001bdbd07242835e53662ae1ba38fcb9dd
shaanvahora50-source <shaanvahora50@gmail.com>
2026-06-09 14:57:25 +0530

    Deleted some file

--- eligibility_checker.py
+++
@@ -1,8 +0,0 @@
-age = int(input("enter your age: "))
-has_id = input("Do you have a valid id? (yes/no): ").lower()
-
-if age > 18 and  has_id == "yes":
- print("eligible")
-
-else:
- print("not aligible")
--- even_odd.py
+++
@@ -1,6 +0,0 @@
-number = int(input("enter number: "))
-
-if number % 2 == 0:
-    print("even")
-else :
-    print("odd") 
